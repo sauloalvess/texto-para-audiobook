2ab292f8b23076cd98e213da27ba8546f68b11d6
Saulo Alves <sauloalves@live.com>
2023-01-19 15:52:51 -0300

    v3

--- main.py
+++ main.py
@@ -1,4 +1,15 @@
 import pyttsx3
 import PyPDF2
 
-ler_arquivo = open()
+ler_arquivo = open("C:\Users\saulo\Downloads\i-am-malala.pdf", "rb")
+livro = PyPDF2.PdfFileReader(ler_arquivo)
+paginas = livro.numPages
+
+voz = pyttsx3.init('sapi5')
+voz.setProperty('rate', 150)
+
+#pagina = livro.getPage(0)
+
+for i in range(0, paginas):
+    texto = paginas[i]
+    voz.save_to_file(texto, "audiobook.mp3")

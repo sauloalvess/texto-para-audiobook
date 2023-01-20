60cb8f623f08726d921c3c35b6880c771ec755a9
Saulo Alves <sauloalves@live.com>
2023-01-20 14:55:33 -0300

    v5 funcionando. Porém, arquivo de teste com biblioteca PyPDF3 não funcionando.

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 import pyttsx3
 import PyPDF2
 
-caminho = open('i-am-malala.pdf', 'rb')
-leitor = PyPDF2.PdfReader(caminho)
-pagina = leitor.pages[0]
+caminho = open('i-am-malala-1.pdf', 'rb') #local e nome do arquivo, no caso na mesma pasta
+leitor = PyPDF2.PdfReader(caminho) #função de leitura do PDF
+pagina = leitor.pages[0] #pega a primeira página
 
-voz = pyttsx3.init('sapi5')
-voz.setProperty('rate', 150)
-completo = "Audiobook"
+voz = pyttsx3.init('sapi5') #função de voz
+voz.setProperty('rate', 150) #propriedades de voz
+completo = ""
 
-for pagina in leitor.pages:
+#loop para passar em todas as páginas, extrair o texto e adicionar ao arquivo
+for pagina in leitor.pages: 
     texto = pagina.extract_text()
     completo = completo + texto
     voz.save_to_file(completo, "audiobook.mp3")
--- test.py
+++ test.py
@@ -1,19 +1,16 @@
 import pyttsx3
-import PyPDF2
-import pdfplumber
+import PyPDF3
 
-arquivo = open('i-am-malala.pdf', 'rb')
-leitor = PyPDF2.PdfReader(arquivo)
-num_pagina = leitor.pages
+caminho = open('i-am-malala-1.pdf', 'rb')
+leitor = PyPDF3.PdfFileReader(caminho)
+pagina = leitor.numPages
 
 voz = pyttsx3.init('sapi5')
 voz.setProperty('rate', 150)
-completo = ""
+completo = "Audiobook"
 
-with pdfplumber.open(arquivo) as pdf:
-    for i in range(num_pagina):
-        pagina = pdf.pages[i]
-        texto = pagina.extract_text()
-        completo += texto
-        voz.save_to_file(completo, "audiobook.mp3")
-        voz.runAndWait()
+for pagina in leitor.pages:
+    texto = pagina.extractText()
+    completo = completo + texto
+    voz.save_to_file(completo, "audiobookpypdf3.mp3")
+    voz.runAndWait()
